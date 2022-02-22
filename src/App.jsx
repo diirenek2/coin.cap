a1070d514d6abd857e204b5a55b96d3f0955a21e
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2022-02-22 01:22:36 -0300

    iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Modal } from './components/Modal'
+import { PlusCircleIcon } from '@heroicons/react/outline'
+
 
 const App = () => {
 
   const [openModal, setOpenModal] = useState(false)
-  const handleAddTransaction = () => {
-    setOpenModal(true)
-  }
 
   return (
     <>
       <Navbar />
 
-      <button className="fixed bottom-4 right-4 bg-yellow-500 hover:bg-yellow-400 active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
+      <button className="fixed bottom-4 right-4 bg-yellow-500 hover:bg-yellow-400 active:bg-yellow-600 font-bold uppercase text-sm px-2 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
         type="button"
-        onClick={handleAddTransaction}
+        onClick={() => setOpenModal(true)}
       >
-        <i className="fas fa-plus"></i>
+        <PlusCircleIcon className="h-9 w-9" aria-hidden="true" />
       </button>
 
       <Modal 
