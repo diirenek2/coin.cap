ee88284667de2ba79f6871955e7dcb760e1200c4
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2022-02-21 22:13:52 -0300

    oscuro y funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 const App = () => {
+
+  const [modal, setModal] = useState(false)
+
+  const handleModal = () => {
+    setModal(true)
+  }
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
+
+      <button className="fixed bottom-4 right-4 bg-yellow-500 hover:bg-yellow-400 text-white active:bg-yellow-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
+        type="button"
+      >
+        <i className="fas fa-plus"></i>
+      </button>
     </>
   )
 }
