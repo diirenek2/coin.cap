312612bd7685e174e937a7545030476ab38b031d
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2022-04-14 02:53:41 -0300

    more coins

--- src/data/coins.js
+++ src/data/coins.js
@@ -16,14 +16,15 @@ const coinsJson = [
   },
   {
     id: 4,
-    name: 'LUNA',
-    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCLUNA.svg',
+    name: 'BNB',
+    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCBNB.svg',
   },
   {
     id: 5,
-    name: 'CHR',
-    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCCHR.svg',
+    name: 'LUNA',
+    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCLUNA.svg',
   },
+
   {
     id: 6,
     name: 'CAKE',
@@ -33,6 +34,21 @@ const coinsJson = [
     id: 7,
     name: 'SLP',
     image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCSLP.svg',
+  },
+  {
+    id: 9,
+    name: 'CHR',
+    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCCHR.svg',
+  },
+  {
+    id: 10,
+    name: 'CHZ',
+    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCCHZ.svg',
+  },
+  {
+    id: 11,
+    name: 'TROY',
+    image:'https://s3-symbol-logo.tradingview.com/crypto/XTVCTROY.svg',
   }
 ]
 
